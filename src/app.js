9a3016991e90d53fadb6ec35ad0cd2a24b6f18aa
Harsh Pawar <pawarharsh21@gmail.com>
2021-03-28 18:55:35 +0530

    add real time functionlaity
    add join room functionality, send canvas data, clear canvas, leave room

--- src/app.js
+++ src/app.js
@@ -1,4 +1,51 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -27,10 +74,64 @@ var io = require('socket.io')(server, {
         methods: ["GET", "POST"]
     }
 });
+var rooms = [];
 // socket connection 
 io.on('connection', function (socket) {
     console.log('user connected');
-    socket.on('canvas-data', function (data) {
-        socket.broadcast.emit("canvas-data", data);
+    socket.on('join-room', function (_a) {
+        var userId = _a.userId, username = _a.username, roomName = _a.roomName;
+        return __awaiter(void 0, void 0, void 0, function () {
+            var removeUserFromRoom, room, isUserAlreadyPresent, isRoomPresent;
+            return __generator(this, function (_b) {
+                socket.join(roomName);
+                socket.to(roomName).on('canvas-data', function (canvasElement) {
+                    // const room = rooms.find(room => room.roomName == roomName);
+                    rooms.map(function (room) {
+                        if (room.roomName === roomName)
+                            room.canvasElements = canvasElement;
+                    });
+                    socket.to(roomName).broadcast.emit("canvas-data", rooms.find(function (room) { return room.roomName === roomName; }));
+                });
+                socket.to(roomName).on('clear-canvas', function () {
+                    rooms.map(function (room) {
+                        if (room.roomName === roomName)
+                            room.canvasElements = [];
+                    });
+                    socket.to(roomName).broadcast.emit('clear-canvas');
+                });
+                socket.to(roomName).on('leave-room', function () {
+                    removeUserFromRoom();
+                    socket.to(roomName).emit('room-users', rooms.find(function (room) { return room.roomName === roomName; }));
+                });
+                socket.on('disconnect', function () {
+                    removeUserFromRoom();
+                    socket.to(roomName).emit('room-users', rooms.find(function (room) { return room.roomName === roomName; }));
+                });
+                removeUserFromRoom = function () {
+                    console.log('leave room');
+                    rooms = rooms.map(function (room) {
+                        return room.roomName === roomName ? __assign(__assign({}, room), { users: room.users.filter(function (user) { return user.userId !== userId; }) }) : room;
+                    });
+                };
+                room = rooms.find(function (room) { return room.roomName === roomName; });
+                isUserAlreadyPresent = room && room.users.some(function (user) { return user.userId === userId; });
+                if (!isUserAlreadyPresent) {
+                    console.log('user joined', roomName, username);
+                    isRoomPresent = rooms.some(function (room) { return room.roomName === roomName; });
+                    isRoomPresent && rooms.map(function (room) {
+                        if (room.roomName === roomName) {
+                            room.users.push({ userId: userId, username: username });
+                        }
+                    });
+                    !isRoomPresent && rooms.push({
+                        roomName: roomName,
+                        users: [{ userId: userId, username: username }],
+                        canvasElements: [],
+                    });
+                }
+                io.to(roomName).emit('room-joined', rooms.find(function (room) { return room.roomName === roomName; }));
+                return [2 /*return*/];
+            });
+        });
     });
 });
--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import http from 'http';
 
 // app config
 const app = express();
@@ -28,11 +27,71 @@ const io = require('socket.io')(server, {
     }
 });
 
+let rooms = [];
+
 // socket connection 
 io.on('connection', (socket) => {
     console.log('user connected');
 
-    socket.on('canvas-data', (data) => {
-        socket.broadcast.emit("canvas-data", data);
+    socket.on('join-room', async ({ userId, username, roomName }) => {
+
+        socket.join(roomName);
+
+        socket.to(roomName).on('canvas-data', (canvasElement) => {
+            // const room = rooms.find(room => room.roomName == roomName);
+            rooms.map(room => {
+                if (room.roomName === roomName)
+                    room.canvasElements = canvasElement;
+            })
+            socket.to(roomName).broadcast.emit("canvas-data", rooms.find(room => room.roomName === roomName));
+        })
+
+        socket.to(roomName).on('clear-canvas', () => {
+            rooms.map(room => {
+                if (room.roomName === roomName)
+                    room.canvasElements = [];
+            })
+            socket.to(roomName).broadcast.emit('clear-canvas');
+        })
+
+        socket.to(roomName).on('leave-room', () => {
+            removeUserFromRoom();
+            socket.to(roomName).emit('room-users', rooms.find(room => room.roomName === roomName))
+        })
+
+        socket.on('disconnect', () => {
+            removeUserFromRoom()
+            socket.to(roomName).emit('room-users', rooms.find(room => room.roomName === roomName))
+        })
+
+        const removeUserFromRoom = () => {
+            console.log('leave room');
+            rooms = rooms.map(room => {
+                return room.roomName === roomName ? {
+                    ...room,
+                    users: room.users.filter(user => user.userId !== userId)
+                } : room;
+            })
+        }
+
+        const room = rooms.find(room => room.roomName === roomName);
+        const isUserAlreadyPresent = room && room.users.some(user => user.userId === userId);
+        if (!isUserAlreadyPresent) {
+            console.log('user joined', roomName, username);
+
+            const isRoomPresent = rooms.some(room => room.roomName === roomName);
+            isRoomPresent && rooms.map(room => {
+                if (room.roomName === roomName) {
+                    room.users.push({ userId, username });
+                }
+            });
+
+            !isRoomPresent && rooms.push({
+                roomName,
+                users: [{ userId, username }],
+                canvasElements: [],
+            })
+        }
+        io.to(roomName).emit('room-joined', rooms.find(room => room.roomName === roomName));
     })
 })
